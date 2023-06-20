701a3f5ef7d54b946e421cd6716f070e6bfe4e5d
Akash221997 <114996063+Akash221997@users.noreply.github.com>
2023-06-20 12:01:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 app.get('/', (req, res) => {
-    res.send("Hello World from Nishant");
+    res.send("Hello World from India");
 })
 app.listen( PORT, () => console.log("Server is listening to port" + PORT ));
